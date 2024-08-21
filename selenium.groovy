c2796165bf0212cab5ef9ebda6b7acd634e7ac50
belaletech <belalahmad@lambdatest.com>
2024-08-21 15:29:00 +0530

    updata

--- selenium.groovy
+++ selenium.groovy
@@ -7,18 +7,33 @@ pipeline {
                 git 'https://github.com/belaletech/selenium_Maven.git'
             }
         }
-        stage('Build code')
-                {
-
-                    steps{
-                        bat script:'mvn compile'
-                    }
-                }
-        stage('Run Test')
-                {
-                    steps{
-                        bat script:'mvn test -Dbrowser=localchrome'
-                    }
-                }
+        stage('Build Code') {
+            steps {
+                bat script: 'mvn compile'
+            }
+        }
+        stage('Run Tests') {
+            steps {
+                bat script: 'mvn test -Dbrowser=localchrome'
+            }
+        }
+        stage('Generate Report') {
+            steps {
+                bat script: 'mvn surefire-report:report-only'
+                bat script: 'mvn site -DgenerateReports=false'
+            }
+        }
+        stage('Publish Report') {
+            steps {
+                publishHTML(target: [
+                        allowMissing: false,
+                        alwaysLinkToLastBuild: true,
+                        keepAll: true,
+                        reportDir: 'target/site',
+                        reportFiles: 'surefire-report.html',
+                        reportName: 'Test Report'
+                ])
+            }
+        }
     }
 }
